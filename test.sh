c8bddbc3a5ab2fb9855a463cf32f6f5303e57757
Hemendra Chaudhary <hemendrachaudhary2052000@gmail.com>
2021-08-16 13:00:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,6 @@ echo 'this for testing'
 date
 #sudo mkdir /root/test
 sudo pwd
-echo 'testing for build triger'
-echo 'test for poll scm'
-echo 'test for github webhook'
-echo 'test for github webhook 2'
-echo 'test for token'
+echo "Check for WebHook Working or Not"
 
 
