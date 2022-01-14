4bd5625c567de37850551aac14a718ac7511041a
Puneet Grover <grover.puneet1995@gmail.com>
2022-01-14 11:47:46 +0000

    14 jan commit 4 folder2

--- folder1/index.py
+++ folder1/index.py
@@ -5,6 +5,7 @@
 # print("Hello, world!!")
 
 import subprocess
+
 domain = input("Enter the Domain: ")
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 print(output)
