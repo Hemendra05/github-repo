c85519ac9c0699a3e1d3da632a34ad1516216d84
Puneet Grover <grover.puneet1995@gmail.com>
2022-01-14 10:54:04 +0000

    14 jan Commit 4 folder1

--- folder1/index.py
+++ folder1/index.py
@@ -1,5 +1,10 @@
 # This program prints Hello, world!
 
-print("Hello, world!!!!")
-print("Hello, world!!")
-print("Hello, world!!")
+# print("Hello, world!!!!")
+# print("Hello, world!!")
+# print("Hello, world!!")
+
+import subprocess
+domain = input("Enter the Domain: ")
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+print(output)
